865b43be28cb5d521fbb90adf232a98f8bfb7acf
Chau Tran <nartc7789@gmail.com>
2024-05-20 22:40:30 -0500

    rotate the walls a bit

--- src/app/gallery/walls.component.ts
+++ src/app/gallery/walls.component.ts
@@ -6,22 +6,19 @@ import { injectNgtsGLTFLoader } from 'angular-three-soba/loaders'
 	selector: 'app-walls',
 	standalone: true,
 	template: `
-		<ngt-primitive *args="[model()]" />
+		<ngt-primitive *args="[model()]" [scale]="3" [rotation]="[0, (Math.PI / 3) * 2, 0]" [position]="[-1.5, 0, 0]" />
 	`,
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [NgtArgs],
 })
 export class Walls {
+	protected Math = Math
+
 	private gltf = injectNgtsGLTFLoader(() => 'models/galleryInnerWalls.glb')
 	protected model = computed(() => {
 		const gltf = this.gltf()
 		if (!gltf) return null
-
-		const scene = gltf.scene
-
-		scene.scale.setScalar(3)
-
-		return scene
+		return gltf.scene
 	})
 }
